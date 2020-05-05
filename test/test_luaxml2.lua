87bb05c65712671313693b48832858def5f8a3b7
JP Florijn <c.florijn@plugwise.com>
2020-05-05 21:46:38 +0200

    Unified memleak and regular tests.

--- test/test_luaxml2.lua
+++ test/test_luaxml2.lua
@@ -1,15 +1,19 @@
---[[
-file:       test_luaxml2.lua
-author:     JP Florijn (JPF), <c.florijn@plugwise.com>
-created:    11/21/2011 08:06:55 PM CET
---]]
 
-require "luaxml2"
+local max = tonumber(arg[1]) or 1
 
-print("Testing luaxml2.validateRelaxNG()\n")
-local ok, err = luaxml2.validateRelaxNG(io.open("test/valid_address_book.xml", "r"):read("*all"), "test/address_book_pattern.rng")
-assert(ok == true, err)
-local ok, err = luaxml2.validateRelaxNG(io.open("test/invalid_address_book.xml", "r"):read("*all"), "test/address_book_pattern.rng")
-assert(ok == false, "This XML document should not be validated by the schema.")
+for i=1, max do
+    if max > 1 and 0 == i % 100 then
+        print(string.format("\t\t%5.2f%% done, %4i to go.", 100 * (i / max), max - i))
 
-print("\tSuccess!\n")
+        -- Tear down on every 100th go, to amplify any possible errors caused by lib loading.
+        package.loaded.luaxml2 = nil
+        luaxml2 = nil
+    end
+    require 'luaxml2'
+    local ok, err = luaxml2.validateRelaxNG(io.open("test/valid_address_book.xml", "r"):read("*all"), "test/address_book_pattern.rng")
+    assert(ok == true, err)
+    local ok, err = luaxml2.validateRelaxNG(io.open("test/invalid_address_book.xml", "r"):read("*all"), "test/address_book_pattern.rng")
+    assert(ok == false, "This XML document should not be validated by the schema.")
+end
+
+print("\n\tSuccess!\n")
--- test/test_luaxml2_memoryleaks.sh
+++ test/test_luaxml2_memoryleaks.sh
@@ -7,5 +7,7 @@ assert_command_exists () {
 assert_command_exists valgrind
 assert_command_exists lua
 
-valgrind --leak-check=full lua test/test_luaxml2_memoryleaks.lua
+NUM_RUNS=2500
+echo -e "\n\tTesting luaxml2.validateRelaxNG() by running it $NUM_RUNS times, while using valgrind to check for memory leaks.\n"
 
+valgrind --leak-check=full lua test/test_luaxml2.lua "$NUM_RUNS"
